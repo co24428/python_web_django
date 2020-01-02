fba8083bbe1f5a2d716eb7a12cd2363190efcd73
ihwan <ihwan1236@gmail.com>
2020-01-02 10:24:28 +0900

    model 추가

--- django/web1/board/models.py
+++ django/web1/board/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Table1(models.Model):
+    object  = models.Manager() # vs code 오류 밑줄 제거용, 안써도 된다.
+
+    no      = models.AutoField(primary_key=True)
+    title   = models.CharField(max_length=200)
+    content = models.TextField()
+    writer  = models.CharField(max_length=50)
+    hit     = models.IntegerField()
+    regdate = models.DateField(auto_now_add=True)
