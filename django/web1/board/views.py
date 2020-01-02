0f51147038b5230b7e8836179c1404fcbd6a7a61
ihwan <ihwan1236@gmail.com>
2020-01-02 09:19:46 +0900

    게시판 초기 설정

--- django/web1/board/views.py
+++ django/web1/board/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+#SQLite 연결
+from django.db import connection
+cursor = connection.cursor()
 
-# Create your views here.
+@csrf_exempt
+def write(request):
+    if request.method=='GET':
+        return render(request, 'board/write.html')
+    if request.method=='POST':
+        pass
